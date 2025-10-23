7147607a7c8ad429ab5a7ccbf8f5549ca6548f29
hamaad3112 <hammad@growmify.com>
2025-10-23 15:57:14 +0500

    next app

--- src/app/layout.js
+++ src/app/layout.js
@@ -38,7 +38,7 @@ export default function RootLayout({ children }) {
       <body className={`${geistSans.variable} ${geistMono.variable}`}> 
         <Header /> 
         <div className="loader-mask">
-          <div className="loader">
+          <div className="loader"> 
             <div className="dot-spinner">
               <div className="dot-spinner__dot"></div>
               <div className="dot-spinner__dot"></div>
