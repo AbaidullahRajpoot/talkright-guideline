a21268d6a6028fbfe8b6cfbcd5d64522e2952fca
Hammix3112 <hamaadjanjua64@gmail.com>
2026-01-17 14:14:57 +0500

    changed class to classname

--- src/app/thanks/page.jsx
+++ src/app/thanks/page.jsx
@@ -5,26 +5,34 @@ const page = () => {
   return (
     <div className="row sections">
       <Sidebar />
-      <div class="col-xl-9 col-md-12">
-        <div class="gx-block" id="introduction">
-          <div class="gx-card gx-page-block">
-            <div class="gx-card-content max-height-data ">
-              <div class="intero centeral">
-                <p class="client-sent">Thank you</p>
-                <p class="popins-data-helpex mt-1">
+      <div className="col-xl-9 col-md-12">
+        <div className="gx-block" id="introduction">
+          <div className="gx-card gx-page-block">
+            <div className="gx-card-content max-height-data ">
+              <div className="intero centeral">
+                <p className="client-sent">Thank you</p>
+                <p className="popins-data-helpex mt-1">
                   Thank you again for download 'TalkRight', enjoy it and give us
                   your valuable Review & Rating.
                 </p>
                 <img
                   src="assets/image/review/Review.gif"
                   alt="review"
-                  class="img-fluid review-sent"
+                  className="img-fluid review-sent"
                 />
-                <div class="review-button">
-                  <a href="#" target="_blank" class="button-buy date-button">
+                <div className="review-button">
+                  <a
+                    href="#"
+                    target="_blank"
+                    className="button-buy date-button"
+                  >
                     Rate This Item
                   </a>
-                  <a href="#" target="_blank" class="button-buy date-button">
+                  <a
+                    href="#"
+                    target="_blank"
+                    className="button-buy date-button"
+                  >
                     Follow Us
                   </a>
                 </div>
